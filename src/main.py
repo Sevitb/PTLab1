0952cb691de26e8da05642852b95c54e73ee98f9
Nvlyrs <peskovatzkov.vs@gmail.com>
2023-12-17 14:28:35 +0300

    My first commit try 3

--- src/main.py
+++ src/main.py
@@ -2,8 +2,10 @@
 import argparse
 import sys
 
+
 from CalcRating import CalcRating
 from TextDataReader import TextDataReader
+
 
 def get_path_from_arguments(args) -> str:
     parser = argparse.ArgumentParser(description="Path to datafile")
@@ -12,15 +14,17 @@ def get_path_from_arguments(args) -> str:
     args = parser.parse_args(args)
     return args.path
 
+
 def main():
     path = get_path_from_arguments(sys.argv[1:])
-    
+
     reader = TextDataReader()
     students = reader.read(path)
     print("Students: ", students)
 
     rating = CalcRating(students).calc()
     print("Rating: ", rating)
-    
+
+
 if __name__ == "__main__":
     main()
